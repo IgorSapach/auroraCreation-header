ef3ea2c159bd6e0dfdd6caaf81082eb8a72c02dd
IgorSapach <igor.sapach7@gmail.com>
2020-12-28 02:51:51 +0300

    added project + text content

---
+++ aurora-project/src/components/header/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <header-top class="header__top" />
+    <img src="/logo__aurora-box.png" />
+    <div><input placeholder="Co chcesz kupić?" /></div>
+    <div>
+      <span><img src="/icon__basket.png" />Twój koszyk</span>
+    </div>
+    <header-nav />
+  </header>
+</template>
+<script>
+import HeaderNav from "./HeaderNav";
+import HeaderTop from "./HeaderTop";
+export default {
+  components: {
+    HeaderNav,
+    HeaderTop,
+  },
+};
+</script>
+<style scoped>
+.header__top {
+  border-bottom: 1px gray solid;
+}
+</style>
---
+++ aurora-project/src/components/header/HeaderNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <div>Kategorie</div>
+    <div>Nowości</div>
+    <div>PROMOCJE</div>
+    <div>WYPRZEDAżE</div>
+    <div>KOLEKCJE SEZONY</div>
+    <div>NASZA OFRETA</div>
+    <div>TRENDY 2018</div>
+    <div>BLOG</div>
+    <div>KONTAKT</div>
+  </nav>
+</template>
